a97cb7745d5f40460e8b25dad866c22e86ef1e0e
Claire Novotny <claire@novotny.org>
2025-09-18 11:24:48 -0400

    Auto add and activate tab

--- MonacoEditorTestApp.Wasm/MainPage.xaml.cs
+++ MonacoEditorTestApp.Wasm/MainPage.xaml.cs
@@ -1,25 +1,4 @@
-﻿using Microsoft.UI.Xaml.Controls;
-using Monaco;
-using Monaco.Editor;
-using Monaco.Helpers;
-using MonacoEditorTestApp.Actions;
-using MonacoEditorTestApp.Helpers;
-using System;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Linq;
-using Windows.Storage;
-using Windows.UI;
-using Windows.UI.Core;
-using Windows.UI.Popups;
-using Windows.UI.Text;
-using Microsoft.UI;
-using Microsoft.UI.Text;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Media;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+﻿// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace MonacoEditorTestApp
 {
@@ -42,9 +21,15 @@ namespace MonacoEditorTestApp
             //tabItem.Header = "Original item";
             //tabItem.Content = new EditorControl();
             //editors.TabItems.Add(tabItem);
+            AddEditorTab();
         }
 
         private void TabView_AddTabButtonClick(TabView sender, object args)
+        {
+            AddEditorTab();
+        }
+
+        private void AddEditorTab()
         {
             var tabItem = new TabViewItem();
             tabItem.IconSource = new Microsoft.UI.Xaml.Controls.SymbolIconSource() { Symbol = Symbol.Document };
@@ -52,6 +37,7 @@ namespace MonacoEditorTestApp
             tabItem.Content = new EditorControl();
             editors.TabItems.Add(tabItem);
         }
+
 
         private void TabView_TabCloseRequested(TabView sender, TabViewTabCloseRequestedEventArgs args)
         {
